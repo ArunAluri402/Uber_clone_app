f3b7505e622333c6a09c0baa48a9381bb29b8d69
AluriArun402 <aluri.freelancing@gmail.com>
2022-03-22 13:41:04 +0530

    FirstCommit

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -1,54 +1,44 @@
-import React from 'react'
-import { useEffect } from 'react';
-import tw from "tailwind-styled-components"
-import mapboxgl from '!mapbox-gl';
+import React from "react";
+import { useEffect } from "react";
+import tw from "tailwind-styled-components";
+import mapboxgl from "!mapbox-gl";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ1bjQwMiIsImEiOiJja3ptbmpzeHozcGd3Mm9rdW04Mnd4bnVxIn0.eL7EOvBZ7IxsNFMs9iivMA';
-
-
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYXJ1bjQwMiIsImEiOiJja3ptbmpzeHozcGd3Mm9rdW04Mnd4bnVxIn0.eL7EOvBZ7IxsNFMs9iivMA";
 
 const Map = (props) => {
+  console.log(props);
 
-  console.log(props)
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph",
+      center: [78.9629, 20.5937],
+      zoom: 3,
+    });
+    if (props.pickupCoordinates) {
+      addToMap(map, props.pickupCoordinates);
+    }
+    if (props.dropoffCoordinates) {
+      addToMap(map, props.dropoffCoordinates);
+    }
 
+    if (props.pickupCoordinates && props.dropoffCoordinates) {
+      map.fitBounds([props.pickupCoordinates, props.dropoffCoordinates], {
+        padding: 40,
+      });
+    }
+  }, [props.pickupCoordinates, props.dropoffCoordinates]);
 
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-          container: 'map',
-          style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
-          center: [78.9629, 20.5937],
-          zoom: 3,
-        })
-        if(props.pickupCoordinates){              addToMap(map,props.pickupCoordinates )
-        }
-        if(props.dropoffCoordinates){              addToMap(map,props.dropoffCoordinates )
-        }
+  const addToMap = (map, coordinates) => {
+    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+  };
 
-        if(props.pickupCoordinates&&props.dropoffCoordinates){
-          map.fitBounds([
-            props.pickupCoordinates, props.dropoffCoordinates
-          ], {
-            padding: 40
-          })
-        }
+  return <Wrapper id="map" />;
+};
 
-      },[props.pickupCoordinates,props.dropoffCoordinates]);
+export default Map;
 
-        const addToMap=(map, coordinates) =>{
-          const marker1 = new mapboxgl.Marker()
-            .setLngLat(coordinates)
-            .addTo(map);
-          }
-
-
-
-  return (
-    <Wrapper id= 'map' />
-  )
-}
-
-export default Map
-
-const Wrapper= tw.div`
+const Wrapper = tw.div`
 flex-1 h-1/2
-`
+`;
